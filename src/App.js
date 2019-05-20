e8e6d09c7191009ec2e3e23346d22cc5d35ea655
mapkeji <mapkeji26@gmail.com>
2019-05-20 15:25:40 +0300

    Update example

--- src/App.js
+++ src/App.js
@@ -39,6 +39,66 @@ const App = () => {
     return null;
   };
 
+  const animatedLayout = () => {
+    if (ymaps) {
+      const animatedLayout = ymaps.templateLayoutFactory.createClass(
+        '<div class="placemark"></div>',
+        {
+          build() {
+            animatedLayout.superclass.build.call(this);
+            const element = this.getParentElement().getElementsByClassName(
+              'placemark',
+            )[0];
+            // Если метка выбрана, то увеличим её размер.
+            const size = this.isActive ? 60 : 34;
+            // При задании для метки своего HTML макета, фигуру активной области
+            // необходимо задать самостоятельно - иначе метка будет неинтерактивной.
+            // Создадим фигуру активной области "Круг".
+            const smallShape = {
+              type: 'Circle',
+              coordinates: [0, 0],
+              radius: size / 2,
+            };
+            const bigShape = {
+              type: 'Circle',
+              coordinates: [0, -30],
+              radius: size / 2,
+            };
+            // Зададим фигуру активной области.
+            this.getData().options.set(
+              'shape',
+              this.isActive ? bigShape : smallShape,
+            );
+            // Если метка выбрана, то зададим класс и запустим анимацию.
+            if (this.isActive) {
+              element.classList.add('active');
+              element.style.animation = '.35s show-big-placemark';
+            } else if (this.inited) {
+              element.classList.remove('active');
+              element.style.animation = '.35s show-small-placemark';
+            }
+            if (!this.inited) {
+              this.inited = true;
+              this.isActive = false;
+              // При клике по метке будем перестраивать макет.
+              this.getData().geoObject.events.add(
+                'click',
+                function() {
+                  this.isActive = !this.isActive;
+                  this.rebuild();
+                },
+                this,
+              );
+            }
+          },
+        },
+      );
+
+      return animatedLayout;
+    }
+    return null;
+  };
+
   useEffect(() => {
     if (document.getElementById('balloon') && clusterBalloon) {
       ReactDOM.hydrate(
@@ -82,9 +142,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <div>Test</div>
-      <YMaps query={{ lang: 'ru_RU' }}>
+      <h1>templateLayoutFactory with React</h1>
+      <YMaps>
         <Map
+          width={640}
+          height={480}
           onLoad={ymaps => setYmaps(ymaps)}
           defaultState={mapState}
           modules={[
@@ -117,6 +179,7 @@ const App = () => {
                   index,
                 }}
                 options={{
+                  iconLayout: animatedLayout(),
                   balloonContentLayout: getLayout(InnerLayout, {
                     point,
                     index,
